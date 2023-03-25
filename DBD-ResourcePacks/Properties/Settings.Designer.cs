6cf27510197c70e34a3687b0bc064960acbbbc31
Charzard4261 <CharzardX4261@gmail.com>
2023-03-25 17:27:13 +0000

    Change Version to 0.9.5

--- DBD-ResourcePacks/Properties/Settings.Designer.cs
+++ DBD-ResourcePacks/Properties/Settings.Designer.cs
@@ -43,7 +43,7 @@ namespace DBD_ResourcePackManager.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("4")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5")]
         public int ProgramVersionPatch {
             get {
                 return ((int)(this["ProgramVersionPatch"]));
